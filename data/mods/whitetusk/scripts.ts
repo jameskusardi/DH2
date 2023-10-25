2e2cfe2d0e5a81075f82b6c8d284d00ff02e669e
jameskusardi <risingSolaris@hotmail.com>
2023-10-25 07:23:44 -0700

    Update scripts.ts
    levitate fix

--- data/mods/whitetusk/scripts.ts
+++ data/mods/whitetusk/scripts.ts
@@ -24,8 +24,8 @@ export const Scripts: {[k: string]: ModdedBattleScriptsData} = {
 			const item = (this.ignoringItem() ? '' : this.item);
 			if (item === 'ironball') return true;
 			// If a Fire/Flying type uses Burn Up and Roost, it becomes ???/Flying-type, but it's still grounded.
-			if (!negateImmunity && this.hasType('Flying') && !('roost' in this.volatiles)) return false;
-			if (this.hasAbility('levitate') && !this.battle.suppressingAttackEvents()) return null;
+			if (!negateImmunity && this.hasType('Flying') && !(this.hasType('???') && 'roost' in this.volatiles)) return false;
+			if (this.hasAbility('levitate') && !this.battle.suppressingAbility(this)) return null;
 			if ('magnetrise' in this.volatiles) return false;
 			if ('telekinesis' in this.volatiles) return false;
 			for (const target of this.battle.getAllActive()) {
