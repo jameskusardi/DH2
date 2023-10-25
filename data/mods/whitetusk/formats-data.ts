a45a08e7bbf0b7f4dd6b2801ad6c0f7d53a92e36
jameskusardi <risingSolaris@hotmail.com>
2023-10-25 11:27:56 -0700

    Update formats-data.ts
    doubles

--- data/mods/whitetusk/formats-data.ts
+++ data/mods/whitetusk/formats-data.ts
@@ -1,119 +1,158 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	dustbunnie: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	rebirb: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbird: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbirdviola: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbirdcello: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbirdcontrabass: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbirdacoustic: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbirdelectric: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	strummingbirdbass: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	xylomist: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	yeomelt: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	zoplite: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	yeoxylo: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	xylozop: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	zopyeo: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	xylyeozop: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	xylobone: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	dormirr: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	pufferfinch: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gumbawl: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gumbrawl: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gumbrawlbubble: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gumbrawlfresh: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gnawingbark: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	ironmint: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	carmilitant: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	toughfee: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gasharmoir: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	gumbrawlgachamech: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	tartridge: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	opixsi: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	pinfrino: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	leagle: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	kuadrosin: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	blite: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	bliteblight: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	doctoxin: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	moosquito: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 	parrox: {
 		tier: "White Tusk",
+		doublesTier: "White Tusk",
 	},
 };
